89ab8f0bfe490e877e146fce6ece788e79cb367d
GÜNER ARDA IŞIK <l5819058@std.yildiz.edu.tr>
2022-09-12 22:17:30 +0300

    create design method of component class

--- Component.js
+++ Component.js
@@ -5,6 +5,7 @@ class Component {
         this.childComp;
         this.props = props;
         this.className = className;
+        this.isMounted = false;
         this.children = []
         if (parent !== 'root') {
             let tempChildren = [...parent.children, this]
@@ -18,8 +19,7 @@ class Component {
 
     set state(newState) {
         this._state = newState;
-        this.unmount();
-        this.build()
+        this.update()
     }
 
     set parent(newParent) {
@@ -32,36 +32,52 @@ class Component {
 
 
     build() {
-        console.log(this)
+        console.log('build' + this.className)
         const div = document.createElement('div');
 
         if (this.className) {
             div.className = this.className;
         }
 
-        if (this._state) {
-            console.log(this._state.a)
-            div.innerText = this._state.a
-        }
         this.dom = div;
-        this.mount();
+        this.design();
+    }
+
+    design() {
+        if (this._state) {
+            console.log(this._state)
+            console.log(this.dom)
+            this.dom.insertAdjacentHTML('afterbegin', this._state.a)
+            console.log(this)
+        }
+        if (!this.isMounted) {
+            this.mount()
+
+        }
+    }
+
+    update() {
+        this.design();
     }
 
     mount() {
         if (this._parent === 'root') {
             document.body.appendChild(this.dom);
+            this.isMounted = true
+
             this.mounted()
+            
 
         } else {
             console.log(this._parent, this.className)
             this._parent.dom.appendChild(this.dom);
             console.log('component mount');
-
+            this.isMounted = true
             this.mounted()
 
         }
 
-        this.children.forEach(child => child.build())
+        this.children.forEach(child => child.update())
     }
 
     mounted() {
--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 import htmlTranslater from "./htmlTranslater.js";
 import Component from "./Component.js";
 const body = document.body;
-const div = new Component('root', { a: 4, b: 5 }, 'kaan');
-const div2 = new Component(div, {}, 'arda')
+const div = new Component('root', { a: 4, b: 5 }, 'div');
+const div2 = new Component(div, {}, 'div2')
+const div3 = new Component(div2, {}, 'div3')
+
+
+
 div2.state = {
-    a: 'arda'
+    a: 'div2', 
+    b: 'div3'
+}
+
+div3.state = {
+    a: 'div3'
 }
 
 
 setTimeout(() => {
-    div2.state = {
-        a:'arda changed'
+    div3.state = {
+        a: 'div3',
+        b:'hello'
     }
 }, 3000)
 
-
+setTimeout(() => {
+    div2.state = {
+        a: 'asdfasdf'
+    }
+}, 6000)
